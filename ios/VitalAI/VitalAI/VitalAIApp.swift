3669c8edb34d04580e2c169b17029170dc84d97c
luisamancio <luis.neto@actionstep.com>
2026-03-13 13:29:10 -0300

    Add Xcode project

---
+++ ios/VitalAI/VitalAI/VitalAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalAIApp.swift
+//  VitalAI
+//
+//  Created by Luis Amancio on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VitalAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
